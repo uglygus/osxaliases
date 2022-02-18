61f12695e36d091e38364a631258bc5049951196
uglygus <cooperbattersby@gmail.com>
2022-02-18 07:25:48 -0500

    add setup.py and remove some debug prints

---
+++ osxaliases/__init__.py
@@ -0,0 +1,5 @@
+# __variables__ with double-quoted values will be available in setup.py:
+__version__ = "0.0.1"
+
+
+from .osxaliases import create_osx_alias, is_alias, resolve_osx_alias
---
+++ setup.py
@@ -0,0 +1,23 @@
+import osxaliases
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="osxaliases-pkg-uglygus",  # Replace with your own username
+    version=osxaliases.__version__,
+    author="Example Author",
+    author_email="author@example.com",
+    description="simplified wrapper for multiprocessing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
